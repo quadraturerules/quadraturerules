8660a1059d05c7475d67c31290bd4527fad2bd4f
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2025-01-14 17:48:19 +0000

    update version number and add test (#20)
    * update version number and add test
    
    * update

--- test/test_numbering.py
+++ test/test_numbering.py
@@ -27,3 +27,19 @@ def test_numbering():
     assert set(qr_files) == set(folders)
     for i in range(1, len(qr_files) + 1):
         assert "Q" + f"000000{i}"[-6:] in qr_files
+
+
+def test_version():
+    folder = join(
+        os.path.dirname(os.path.realpath(__file__)),
+        "..",
+        "rules",
+    )
+    v = max(int(file[1:-3]) for file in os.listdir(folder) if file.endswith(".qr"))
+
+    with open(join(
+        os.path.dirname(os.path.realpath(__file__)),
+        "..",
+        "VERSION",
+    )) as f:
+        assert int(f.read().split(".")[1]) == v
