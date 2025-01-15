f9a9638572a070afb355795401f09bd6627d449f
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2025-01-15 12:10:38 +0000

    updates (#23)

--- website/build.py
+++ website/build.py
@@ -6,10 +6,9 @@ import re
 from datetime import datetime
 
 from quadraturerules_website import settings
-from quadraturerules_website.markup import markup
 from quadraturerules_website.rules import load_rule
 from webtools.html import make_html_page
-from webtools.markup import heading, heading_with_self_ref
+from webtools.markup import heading, heading_with_self_ref, markup
 from webtools.tools import html_local, parse_metadata
 
 start_all = datetime.now()
