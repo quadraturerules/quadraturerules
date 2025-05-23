44a53da8a8b32b33083c8d4b57e90e87d8bfd1f9
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2025-05-23 07:17:03 +0100

    add test that volume of cell is 1

---
+++ test/test_unit.py
@@ -0,0 +1,32 @@
+import numpy as np
+import os
+import pytest
+import yaml
+
+from webtools.tools import join
+
+folder = join(
+    os.path.dirname(os.path.realpath(__file__)),
+    "..",
+    "rules",
+)
+rules = []
+for file in os.listdir(folder):
+    if not file.startswith(".") and file.endswith(".qr"):
+        assert os.path.isfile(os.path.join(folder, file))
+        family = file[:-3]
+        for rule in os.listdir(join(folder, family)):
+            if not rule.startswith("."):
+                rules.append((family, rule))
+
+
+@pytest.mark.parametrize(("family", "rule"), rules)
+def test_cell_has_unit_volume(family, rule):
+    with open(join(folder, f"{family}.qr")) as f:
+        info = yaml.load(f, yaml.FullLoader)
+    if info["integrand"] != "f(x)":
+        pytest.xfail()
+
+    with open(join(folder, family, rule)) as f:
+        volume = sum(float(p.split(" | ")[1]) for p in f.read().split("--")[2].strip().split("\n"))
+    assert np.isclose(volume, 1.0)
