da39e2722bd2a0ec1e27e240c61686a225be762c
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2025-08-05 08:30:54 +0100

    add test against quadpy

---
+++ python/test/test_against_quadpy.py
@@ -0,0 +1,105 @@
+import numpy as np
+import os
+import pytest
+import yaml
+from webtools.tools import join
+
+try:
+    import quadpy
+
+    has_quadpy = True
+except ImportError:
+    has_quadpy = False
+
+folder = join(
+    os.path.dirname(os.path.realpath(__file__)),
+    "..",
+    "..",
+    "rules",
+)
+
+
+@pytest.mark.parametrize(
+    ("qfolder", "qfile"),
+    [
+        (qfolder, qfile)
+        for qfolder in os.listdir(folder)
+        if os.path.isdir(join(folder, qfolder))
+        for qfile in os.listdir(join(folder, qfolder))
+        if qfile.endswith(".rule")
+    ],
+)
+def test_against_quadpy(qfolder, qfile):
+    if not has_quadpy:
+        pytest.skip()
+
+    qpy_map = {
+        "Q000001": {"interval": quadpy.c1.gauss_legendre},
+        "Q000002": {
+            "triangle": lambda order: quadpy.t2.schemes[
+                "xiao_gimbutas_" + f"00{order}"[-2:]
+            ](),
+            "tetrahedron": lambda order: quadpy.t3.schemes[
+                "xiao_gimbutas_" + f"00{order}"[-2:]
+            ](),
+        },
+        "Q000003": {"interval": quadpy.c1.gauss_lobatto},
+        "Q000004": None,
+        "Q000005": None,
+        "Q000006": {
+            "triangle": lambda order: quadpy.t2.schemes[
+                f"hammer_marlowe_stroud_{order}"
+            ](),
+            "tetrahedron": lambda order: quadpy.t3.schemes[
+                f"hammer_marlowe_stroud_{order}"
+            ](),
+        },
+        "Q000007": None,
+    }
+
+    qpy_function = qpy_map[qfolder]
+    if qpy_function is None:
+        pytest.skip()
+
+    with open(join(folder, qfolder, qfile)) as f:
+        _, yaml_data, rule = f.read().split("--\n")
+    info = yaml.load(yaml_data, yaml.FullLoader)
+
+    qpy_scheme = qpy_function[info["domain"]](info["order"])
+
+    pts = []
+    wts = []
+    for line in rule.strip().split("\n"):
+        p, w = line.split(" | ")
+        pts.append([float(i) for i in p.split(" ")])
+        wts.append(float(w))
+
+    match info["domain"]:
+        case "interval":
+            mapped_pts = [p[0] - p[1] for p in pts]
+            volume = 2
+        case "triangle":
+            mapped_pts = pts
+            volume = 1
+        case "tetrahedron":
+            mapped_pts = pts
+            volume = 1
+        case _:
+            raise NotImplementedError()
+
+    order = []
+    for p in mapped_pts:
+        for i, q in enumerate(qpy_scheme.points.T):
+            if np.allclose(p, q):
+                order.append(i)
+                break
+        else:
+            print(f"{p} not found in {qpy_scheme.points.T}")
+            raise ValueError("Points do not match")
+    print(wts * 2)
+    for i, j in enumerate(order):
+        print(wts[j], qpy_scheme.weights[i])
+    for i, j in enumerate(order):
+        assert np.isclose(wts[i] * volume, qpy_scheme.weights[j]), (
+            "Weights do not match"
+        )
